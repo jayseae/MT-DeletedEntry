055a324fe37de63106a9363d5c6c7609e19e62a7
Chad Everett <github@jayseae.cxliv.org>
2009-07-17 10:27:54 -0400

    Update license to Artistic - 0.1.3

--- trunk/plugins/DeletedEntry/DeletedEntry.pl
+++ trunk/plugins/DeletedEntry/DeletedEntry.pl
@@ -1,5 +1,17 @@
 # ===========================================================================
-# Copyright 2006, Everitz Consulting (mt@everitz.com)
+# A Movable Type plugin to remove entry pages from disk on entry deletion.
+# Copyright 2006 Everitz Consulting <everitz.com>.
+#
+# This program is free software:  You may redistribute it and/or modify it
+# it under the terms of the Artistic License version 2 as published by the
+# Open Source Initiative.
+#
+# This program is distributed in the hope that it will be useful but does
+# NOT INCLUDE ANY WARRANTY; Without even the implied warranty of FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+# You should have received a copy of the Artistic License with this program.
+# If not, see <http://www.opensource.org/licenses/artistic-license-2.0.php>.
 # ===========================================================================
 package MT::Plugin::DeletedEntry;
 
@@ -11,10 +23,10 @@ use MT;
 my $DeletedEntry;
 my $about = {
   name => 'MT-DeletedEntry',
-  description => 'Removes entry page from disk on entry deletion.',
+  description => 'Remove entry pages from disk on entry deletion.',
   author_name => 'Everitz Consulting',
-  author_link => 'http://www.everitz.com/',
-  version => '0.1.2'
+  author_link => 'http://everitz.com/',
+  version => '0.1.3'
 };
 $DeletedEntry = MT::Plugin::DeletedEntry->new($about);
 MT->add_plugin($DeletedEntry);
